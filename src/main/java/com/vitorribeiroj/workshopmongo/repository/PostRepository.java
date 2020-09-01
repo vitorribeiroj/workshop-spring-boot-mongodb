685243fbc64814f9b2ca5a08fb9a090b2b9b03bf
Vitor Jardim <vitorribeiroj@gmail.com>
2020-09-01 15:03:16 -0300

    Consulta simples com @Query

--- src/main/java/com/vitorribeiroj/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/vitorribeiroj/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.vitorribeiroj.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.vitorribeiroj.workshopmongo.domain.Post;
@@ -12,5 +13,7 @@ public interface PostRepository extends MongoRepository<Post, String>{
 
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 
 }
