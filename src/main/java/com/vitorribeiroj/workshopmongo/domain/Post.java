04990ef4473902b7b035e7338ac3adbdc3a603d1
Vitor Jardim <vitorribeiroj@gmail.com>
2020-09-01 14:10:49 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/vitorribeiroj/workshopmongo/domain/Post.java
+++ src/main/java/com/vitorribeiroj/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.vitorribeiroj.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.vitorribeiroj.workshopmongo.dto.AuthorDTO;
+import com.vitorribeiroj.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -19,6 +22,7 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -71,6 +75,14 @@ public class Post implements Serializable{
 	public void setBody(String body) {
 		this.body = body;
 	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public int hashCode() {
@@ -96,6 +108,8 @@ public class Post implements Serializable{
 			return false;
 		return true;
 	}
+
+
 	
 	
 	
